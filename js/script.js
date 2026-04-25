f9dc65ac2dfe28598d04c95aa5316f44218a915d
yakiTreeseason <maki05263@gmail.com>
2026-04-25 23:38:26 +0900

    feat:ナビ、スライダー、モーダル

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// モーダル
+function openModal(title, text, imgSrc){
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const modalTitle = document.getElementById("modal-title");
+    const modalText = document.getElementById("modal-text");
+
+    modal.style.display = "flex"; /*画面を出す*/ 
+    modalImg.src = imgSrc;
+    modalTitle.textContent = title; /*タイトルを差し替え*/
+    modalText.textContent = text;
+}
+
+function closeModal(){
+    document.getElementById("modal").style.display = "none"; /*閉じる*/
+}
+// ハンバーガーメニュー
+function toggleMenu(){
+    document.querySelector(".nav").classList.toggle("active");//クラスをON/OFFするスイッチ
+}
+document.querySelectorAll(".nav a").forEach(link => { 
+    link.addEventListener("click",() =>{ //ナビのリンクをクリックすると
+        document.querySelector(".nav").classList.remove("active"); //自動で閉じる
+    });
+});
